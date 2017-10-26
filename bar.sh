3b8f04aa785abd2b6b9a5248588a9eaf22dbf016
wjwGeronimo <root@vzhilkin.itmm.ru>
2017-10-26 13:30:38 +0300

    modified:   bar.sh

--- bar.sh
+++ bar.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-#TOTAL=$2
-#I=$1
-
 barprint(){
 	#tput el
 	echo -n "["
@@ -26,9 +23,6 @@ barprint(){
 	echo "] $PROG $DAT"
 	#tput cuu1
 }
-
-#while [[ $I -le $TOTAL ]]
-#do
 
 onetime(){
 	I=$1
@@ -65,29 +59,3 @@ cycle(){
 		sleep $TIMER
 	done
 }
-
-usage(){
-	#echo "$0 num total to print one time progress bar"
-	echo "$0 [-r timer] start total - to count in cycle"
-}
-
-#if [[ $# -eq 2 ]]
-#then
-#	I=$1
-#	TOTAL=$2
-#	onetime
-#elif [[ $# -gt 2 ]]
-#then
-#	REC=0
-#	while [[ $# -gt 0 ]]
-#	do
-#		case $1 in
-#			-r | --recursive ) REC=1; TIMER=$2; shift 2;;
-#			* ) I=$1; TOTAL=$2; shift 2;;
-#		esac
-#	done
-#	[[ $REC -eq 1 ]]&&cycle
-#	[[ $REC -eq 0 ]]&&onetime
-#else
-#	usage
-#fi
